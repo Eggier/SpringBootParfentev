df226264cd34d2014aadfc9042912739007d1aa7
Eggier <egor_parfentev_23@mail.ru>
2023-03-15 17:31:50 +0500

    Start

---
+++ SpringBootParfentev/src/main/java/com/example/SpringBootParfentev/SpringBootParfentevApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootParfentev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootParfentevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootParfentevApplication.class, args);
+	}
+
+}
---
+++ SpringBootParfentev/src/test/java/com/example/SpringBootParfentev/SpringBootParfentevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootParfentev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootParfentevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
